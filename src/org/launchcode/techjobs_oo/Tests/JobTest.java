ee15913df9b09c6d3c377ef73f82d48085e977e4
farleysm8909 <farleysm8909@uwec.edu>
2021-01-28 19:17:24 -0600

    Halfway through part 4 of A2

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,35 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+
+import static org.junit.Assert.*;
+
+public class JobTest {
+
+    Job job1, job2, job3;
+
+    @Before
+    public void createJobObjects() {
+        job1 = new Job();
+        job2 = new Job();
+        job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    }
+
+    @Test
+    public void testSettingJobId() {
+        assertFalse(job1.getId() == job2.getId()); //confirm IDs are not the same
+        assertTrue((job2.getId() - job1.getId()) >= 1); //ensure IDs are at least 1 apart
+        // OR assertEquals(1, job2.getId() - job1.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        assertEquals(job3.getName(), "Product tester");
+        assertEquals(job3.getEmployer().getValue(), "ACME"); //getEmployer() returns Employer object, must do getValue to return string
+        assertEquals(job3.getLocation().getValue(), "Desert");
+        assertEquals(job3.getPositionType().getValue(), "Quality control");
+        assertEquals(job3.getCoreCompetency().getValue(), "Persistence");
+    }
+}
